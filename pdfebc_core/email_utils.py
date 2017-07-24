f943b09f7bd1d3fd7e0b69f6c21057a20e3e3659
Simon Larsén <slarse@kth.se>
2017-07-24 14:19:54 +0200

    Switch to aiosmtplib, fix #17 (PR #17)
    * Add aiosmtplib to requirements.txt
    
    * Add asynctest to requirements.txt
    
    * Switch to aiosmtplib

--- pdfebc_core/email_utils.py
+++ pdfebc_core/email_utils.py
@@ -11,6 +11,8 @@ for more information.
 .. moduleauthor:: Simon Larsén <slarse@kth.se>
 """
 import smtplib
+import asyncio
+import aiosmtplib
 import os
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
@@ -31,7 +33,7 @@ Files:
 {}"""
 FILES_SENT = "Files successfully sent!"""
 
-def send_with_attachments(subject, message, filepaths, config):
+async def send_with_attachments(subject, message, filepaths, config):
     """Send an email from the user (a gmail) to the receiver.
 
     Args:
@@ -46,7 +48,7 @@ def send_with_attachments(subject, message, filepaths, config):
     email_["From"] = get_attribute_from_config(config, EMAIL_SECTION_KEY, USER_KEY)
     email_["To"] = get_attribute_from_config(config, EMAIL_SECTION_KEY, RECEIVER_KEY)
     _attach_files(filepaths, email_)
-    _send_email(email_, config)
+    await _send_email(email_, config)
 
 def _attach_files(filepaths, email_):
     """Take a list of filepaths and attach the files to a MIMEMultipart.
@@ -62,7 +64,7 @@ def _attach_files(filepaths, email_):
             part["Content-Disposition"] = 'attachment; filename="%s"' % base
             email_.attach(part)
 
-def _send_email(email_, config):
+async def _send_email(email_, config, loop=asyncio.get_event_loop()):
     """Send an email.
 
     Args:
@@ -73,13 +75,14 @@ def _send_email(email_, config):
     smtp_port = int(get_attribute_from_config(config, EMAIL_SECTION_KEY, SMTP_PORT_KEY))
     user = get_attribute_from_config(config, EMAIL_SECTION_KEY, USER_KEY)
     password = get_attribute_from_config(config, EMAIL_SECTION_KEY, PASSWORD_KEY)
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(user, password)
-    server.send_message(email_)
-    server.quit()
+    server = aiosmtplib.SMTP(hostname=smtp_server, port=smtp_port, loop=loop, use_tls=False)
+    await server.connect()
+    await server.starttls()
+    await server.login(user, password)
+    await server.send_message(email_)
+    await server.quit()
 
-def send_files_preconf(filepaths, config_path=CONFIG_PATH):
+async def send_files_preconf(filepaths, config_path=CONFIG_PATH):
     """Send files using the config.ini settings.
 
     Args:
@@ -88,4 +91,4 @@ def send_files_preconf(filepaths, config_path=CONFIG_PATH):
     config = read_config(config_path)
     subject = "PDF files from pdfebc"
     message = ""
-    send_with_attachments(subject, message, filepaths, config)
+    await send_with_attachments(subject, message, filepaths, config)
